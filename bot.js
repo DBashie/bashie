5edc38ebc0f5103bcfc48d78f40547e2168b7aca
DBashie <37782071+DBashie@users.noreply.github.com>
2018-03-25 23:55:01 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+bot.run("NDI3NDE3MjY4Nzk3MDQ2Nzg0.DZk_AA.9D2JiG8Nb88Wigu3fPn6CP_gPUE")
